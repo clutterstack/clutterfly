a5f1264ca20981d1c430689cd3e55bcf30b44c28
Chris Nicoll <5599856+clutterstack@users.noreply.github.com>
2025-03-07 13:07:37 -0500

    rough in validate_and_run fns using Claude

--- lib/client.ex
+++ lib/client.ex
@@ -1,5 +1,4 @@
 defmodule Clutterfly.Client do
-
   import Clutterfly.Validation
 
   require Logger
@@ -11,7 +10,6 @@ defmodule Clutterfly.Client do
   `export FLY_API_TOKEN=$(fly tokens deploy -a <appname>)`
   to get a deploy token for one app.
   """
-
 
   @doc """
   Functions to validate and execute API calls, using the FlyMachines pkg as the core
@@ -34,25 +32,27 @@ defmodule Clutterfly.Client do
   make it significantly harder to read though.
   """
 
- # API calls with just appname path param
+  # API calls with just appname path param
 
   # Set default empty body if omitted
   def validate_and_run(operation, [args]) when is_atom(operation) do
     validate_and_run(operation, [args], %{})
   end
 
-  def validate_and_run(:machine_create, [appname], body) do
-    with {:ok, req_body} <- validate_body(body, Clutterfly.FlySchemas.CreateMachineRequest),
-         {:ok, response} <- FlyMachines.machine_create(appname, req_body) do
-      {:ok, response}
+  # App operations
+  def validate_and_run(:app_create, [], body) do
+    with {:ok, req_body} <- validate_body(body, Clutterfly.FlySchemas.CreateAppRequest),
+         {:ok, response} <- FlyMachines.app_create(req_body) do
+      {:ok, response.body}
     else
       {:error, response} -> {:error, response}
     end
   end
 
-  def validate_and_run(:volume_create, [appname], body) do
-    with {:ok, req_body} <- validate_body(body, Clutterfly.FlySchemas.CreateVolumeRequest),
-         {:ok, response} <- FlyMachines.volume_create(appname, req_body) do
+  # Machine operations
+  def validate_and_run(:machine_create, [appname], body) do
+    with {:ok, req_body} <- validate_body(body, Clutterfly.FlySchemas.CreateMachineRequest),
+         {:ok, response} <- FlyMachines.machine_create(appname, req_body) do
       {:ok, response.body}
     else
       {:error, response} -> {:error, response}
@@ -69,8 +69,224 @@ defmodule Clutterfly.Client do
   end
 
   def validate_and_run(:machine_stop, [appname, machine_id], body) do
-    with {:ok, req_body} <- validate_body(body, Clutterfly.FlySchemas.StopMachineRequest),
+    with {:ok, req_body} <- validate_body(body, Clutterfly.FlySchemas.StopRequest),
          {:ok, response} <- FlyMachines.machine_stop(appname, machine_id, req_body) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_start, [appname, machine_id], _body) do
+    with {:ok, response} <- FlyMachines.machine_start(appname, machine_id) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_restart, [appname, machine_id], _body) do
+    with {:ok, response} <- FlyMachines.machine_restart(appname, machine_id) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_signal, [appname, machine_id], body) do
+    with {:ok, req_body} <- validate_body(body, Clutterfly.FlySchemas.SignalRequest),
+         {:ok, response} <- FlyMachines.machine_signal(appname, machine_id, req_body) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_cordon, [appname, machine_id], _body) do
+    with {:ok, response} <- FlyMachines.machine_cordon(appname, machine_id) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_uncordon, [appname, machine_id], _body) do
+    with {:ok, response} <- FlyMachines.machine_uncordon(appname, machine_id) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_lease_acquire, [appname, machine_id], body) do
+    with {:ok, req_body} <- validate_body(body, Clutterfly.FlySchemas.CreateLeaseRequest),
+         {:ok, response} <- FlyMachines.machine_lease_acquire(appname, machine_id, req_body) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_lease_release, [appname, machine_id, lease_nonce], _body) do
+    with {:ok, response} <- FlyMachines.machine_lease_release(appname, machine_id, lease_nonce) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_wait, [appname, machine_id], params) do
+    # Note: This one doesn't validate the params with a schema
+    with {:ok, response} <- FlyMachines.machine_wait(appname, machine_id, params) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  # Volume operations
+  def validate_and_run(:volume_create, [appname], body) do
+    with {:ok, req_body} <- validate_body(body, Clutterfly.FlySchemas.CreateVolumeRequest),
+         {:ok, response} <- FlyMachines.volume_create(appname, req_body) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:volume_update, [appname, volume_id], body) do
+    with {:ok, req_body} <- validate_body(body, Clutterfly.FlySchemas.UpdateVolumeRequest),
+         {:ok, response} <- FlyMachines.volume_update(appname, volume_id, req_body) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:volume_extend, [appname, volume_id], body) do
+    with {:ok, req_body} <- validate_body(body, Clutterfly.FlySchemas.ExtendVolumeRequest),
+         {:ok, response} <- FlyMachines.volume_extend(appname, volume_id, req_body) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:volume_delete, [appname, volume_id], _body) do
+    with {:ok, response} <- FlyMachines.volume_delete(appname, volume_id) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  # Operations that don't require body validation
+  def validate_and_run(:app_list, [org_slug], _body) do
+    with {:ok, response} <- FlyMachines.app_list(org_slug) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:app_retrieve, [appname], _body) do
+    with {:ok, response} <- FlyMachines.app_retrieve(appname) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:app_delete, [appname], _body) do
+    with {:ok, response} <- FlyMachines.app_delete(appname) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_list, [appname], _body) do
+    with {:ok, response} <- FlyMachines.machine_list(appname) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_retrieve, [appname, machine_id], _body) do
+    with {:ok, response} <- FlyMachines.machine_retrieve(appname, machine_id) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_delete, [appname, machine_id], _body) do
+    with {:ok, response} <- FlyMachines.machine_delete(appname, machine_id) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_ps, [appname, machine_id], _body) do
+    with {:ok, response} <- FlyMachines.machine_ps(appname, machine_id) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_event_list, [appname, machine_id], _body) do
+    with {:ok, response} <- FlyMachines.machine_event_list(appname, machine_id) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_versions_list, [appname, machine_id], _body) do
+    with {:ok, response} <- FlyMachines.machine_versions_list(appname, machine_id) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_metadata_retrieve, [appname, machine_id], _body) do
+    with {:ok, response} <- FlyMachines.machine_metadata_retrieve(appname, machine_id) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:machine_metadata_delete, [appname, machine_id, key], _body) do
+    with {:ok, response} <- FlyMachines.machine_metadata_delete(appname, machine_id, key) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:volume_list, [appname], _body) do
+    with {:ok, response} <- FlyMachines.volume_list(appname) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:volume_retrieve, [appname, volume_id], _body) do
+    with {:ok, response} <- FlyMachines.volume_retrieve(appname, volume_id) do
+      {:ok, response.body}
+    else
+      {:error, response} -> {:error, response}
+    end
+  end
+
+  def validate_and_run(:volume_snapshots_list, [appname, volume_id], _body) do
+    with {:ok, response} <- FlyMachines.volume_snapshots_list(appname, volume_id) do
       {:ok, response.body}
     else
       {:error, response} -> {:error, response}
@@ -81,7 +297,4 @@ defmodule Clutterfly.Client do
   def validate_and_run(operation, _args, _body) do
     {:error, {:unknown_operation, operation}}
   end
-
-  # defp compose_errmsg()
-
 end
