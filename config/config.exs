6431aa5428635dd18840e074694634e120e76f51
Chris Nicoll <5599856+clutterstack@users.noreply.github.com>
2025-03-16 20:19:05 -0400

    move config to runtime since it needs env vars

--- config/config.exs
+++ config/config.exs
@@ -1,12 +1,12 @@
 # General application configuration
 import Config
 
-config :clutterfly,
-  api_token: System.fetch_env!("FLY_API_TOKEN")
+# config :clutterfly,
+#   api_token: System.fetch_env!("FLY_API_TOKEN")
 
 
-# Config for https://github.com/ckreiling/fly_machines API client (based on Req)
-config :fly_machines, default: [
-  base_url: "https://api.machines.dev/v1",
-  auth: {:bearer, System.fetch_env!("FLY_API_TOKEN")}
-]
+# # Config for https://github.com/ckreiling/fly_machines API client (based on Req)
+# config :fly_machines, default: [
+#   base_url: "https://api.machines.dev/v1",
+#   auth: {:bearer, System.fetch_env!("FLY_API_TOKEN")}
+# ]
